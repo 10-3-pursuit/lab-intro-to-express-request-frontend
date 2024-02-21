bcb96518357bac4fe83f2efa8756a38f769d4442
brenda-soto <bsoto@pursuit.org>
2024-02-21 16:46:53 -0500

    Front end working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Pokemon Frontend</h1>
-    </div>
-  )
-}
+  const [pokemon, setPokemon] = useState([]);
 
-export default App
+  useEffect(() => {
+    fetch("http://localhost:3050/pokemon")
+      .then((res) => res.json())
+      .then((data) => setPokemon(data.pokemon));
+  }, []);
+
+  return (
+    <div className="container">
+      {pokemon.map((mon) => (
+        <div className="card" key={mon.img}>
+          <img src={mon.img} alt={mon.name} />
+          <p>Name: {mon.name}</p>
+          <p>Type/s: {mon.type.join(", ")}</p>
+          <p>HP: {mon.stats.hp}</p>
+          <p>Attack: {mon.stats.attack}</p>
+          <p>Defense: {mon.stats.defense}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
